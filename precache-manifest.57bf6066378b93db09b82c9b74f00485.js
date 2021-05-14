fd0f3213bf6ac857c01327036848c9112b177f6d
Elizar Pongracz <pelipongracz@gmail.com>
2021-05-14 16:36:53 +0200

    Updates

---
+++ precache-manifest.57bf6066378b93db09b82c9b74f00485.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e144e74c81a8530178b14da39115e2b4",
+    "url": "/elfolio/index.html"
+  },
+  {
+    "revision": "b60c628f64062ac2f70a",
+    "url": "/elfolio/static/css/2.3a340521.chunk.css"
+  },
+  {
+    "revision": "aba12cff5b3a508e931d",
+    "url": "/elfolio/static/css/main.2521cc89.chunk.css"
+  },
+  {
+    "revision": "b60c628f64062ac2f70a",
+    "url": "/elfolio/static/js/2.afdee29c.chunk.js"
+  },
+  {
+    "revision": "ec7633d1b480fccf0307aeb10b148379",
+    "url": "/elfolio/static/js/2.afdee29c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aba12cff5b3a508e931d",
+    "url": "/elfolio/static/js/main.ad3b8064.chunk.js"
+  },
+  {
+    "revision": "2f13d270070cebf240ea",
+    "url": "/elfolio/static/js/runtime-main.be8c152d.js"
+  },
+  {
+    "revision": "70f40b0f362095d9544ebbd3c8293bf5",
+    "url": "/elfolio/static/media/best-app-development-tool.70f40b0f.md"
+  },
+  {
+    "revision": "bdea94e63d3da5dee4bbf391f6a0a24e",
+    "url": "/elfolio/static/media/common-misconceptions-about-payment.bdea94e6.md"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/elfolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/elfolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/elfolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/elfolio/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "78d248241b422acf5d590bcdda06139c",
+    "url": "/elfolio/static/media/things-to-know-about-startup-business.78d24824.md"
+  }
+]);
