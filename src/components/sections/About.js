2ba6b7637098cf66978aad11bd572463b4ff3330
Elizar Pongracz <pelipongracz@gmail.com>
2021-05-15 09:37:32 +0200

    added avatar

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -9,7 +9,7 @@ var end = moment();
 
 const aboutContent = {
     name: "Elizar Pongracz",
-    avatarImage: "images/avatar-2.svg",
+    avatarImage: "images/avatar-1.svg",
     content:
         "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
 };
--- src/components/sections/Contact.js
+++ src/components/sections/Contact.js
@@ -29,6 +29,7 @@ function Contact() {
             setMessage("Message is required");
         } else {
             setError(false);
+
             setMessage("You message has been sent!!!");
         }
     };
